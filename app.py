60c1c771501aa23cfe0be2a872e644926f2c2c6e
zachbush96 <zachbush96@gmail.com>
2025-08-24 21:29:29 -0400

    Add .env to gitignore

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import os
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 app.secret_key = 'change_this_secret_key'  # Needed for session
+#airtable api key from .env
+load_dotenv()
+airtable_api_key = os.getenv('AIRTABLE_API_KEY')
+airtable_base_id = os.getenv('AIRTABLE_BASE_ID')
 
 
 @app.route('/')
@@ -34,6 +41,36 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
 
+
+@app.route('/admin/leads')
+def leads():
+    # An endpoint to return all the leads and details about them
+    # This information should be pulled from airtable
+    # This information should be returned in a json format
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    
+    #get all the leads from airtable
+    url = f'https://api.airtable.com/v0/{airtable_base_id}/Leads'
+    headers = {
+        'Authorization': f'Bearer {airtable_api_key}'
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+@app.route('/admin/businesses')
+def businesses():
+    # An endpoint to return all the businesses and details about them
+    # This information should be pulled from airtable
+    # This information should be returned in a json format
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    url = f'https://api.airtable.com/v0/{airtable_base_id}/Businesses'
+    headers = {
+        'Authorization': f'Bearer {airtable_api_key}'
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
 
 if __name__ == '__main__':
     app.run(debug=True)
